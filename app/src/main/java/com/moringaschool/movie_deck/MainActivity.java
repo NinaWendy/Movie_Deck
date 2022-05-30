8c67c80649ae23f4494e8f29709466912e763a6d
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-05-30 21:56:12 +0300

    Add butterknife dependency and set viewbinding to true

--- app/src/main/java/com/moringaschool/movie_deck/MainActivity.java
+++ app/src/main/java/com/moringaschool/movie_deck/MainActivity.java
@@ -2,13 +2,28 @@ package com.moringaschool.movie_deck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.explore) Button mExploreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mExploreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
