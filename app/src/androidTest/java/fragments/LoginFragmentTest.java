27ffc5df34ed1ea7f7e5362472d980eb6007fee2
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-06-01 07:16:10 +0300

    Add google truth dependency

---
+++ app/src/androidTest/java/fragments/LoginFragmentTest.java
@@ -0,0 +1,22 @@
+package fragments;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class LoginFragmentTest {
+
+    @Test
+    public void validateUserInput_returnTrue() {
+        String  user ="Wendy";
+        assertTrue(validateUser(user));
+    }
+//helper
+    public Boolean validateUser(String user){
+    if(user.isEmpty() || user.length() <2){
+        return Boolean.FALSE;
+
+    }else
+        return Boolean.TRUE;
+}}
--- app/src/main/java/fragments/LoginFragment.java
+++ app/src/main/java/fragments/LoginFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.moringaschool.movie_deck.R;
@@ -27,16 +28,30 @@ public class LoginFragment extends Fragment {
 
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false);
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-//                   String user = username.getText().toString();
-                Toast.makeText(getActivity(), "You are logged in!", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(getActivity(), SearchActivity.class);
-                // intent.putExtra("user",user);
-                startActivity(intent);
+                String username = binding.username.getEditText().getText().toString();
+                validateUser(username);
+                if (validateUser(username)) {
+                    Toast.makeText(getActivity(), "You are logged as " + username.toString(), Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getActivity(), SearchActivity.class);
+                    startActivity(intent);
+                }
+            }
+            public Boolean validateUser(String user){
+                if(user.isEmpty() || user.length() <2){
+
+                    Toast.makeText(getActivity(), "invalid user name", Toast.LENGTH_LONG).show();
+                    return Boolean.FALSE;
+
+                }else
+                    return Boolean.TRUE;
             }
         });
         // Inflate the layout for this fragment
+
+
         return binding.getRoot();
     }
 }
