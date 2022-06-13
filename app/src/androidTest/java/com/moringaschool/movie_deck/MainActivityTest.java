4a4ac03ee86c0bb18be433bcef7a0d1b50cb0c16
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-06-13 13:23:27 +0300

    Add tests

---
+++ app/src/androidTest/java/com/moringaschool/movie_deck/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.moringaschool.movie_deck;
+
+import static org.junit.Assert.*;
+
+import android.view.View;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class MainActivityTest {
+
+}
---
+++ app/src/test/java/com/moringaschool/movie_deck/LoginActivityUnitTest.java
@@ -0,0 +1,14 @@
+package com.moringaschool.movie_deck;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.moringaschool.movie_deck.ui.activities.LoginActivity;
+
+import org.junit.Test;
+
+public class LoginActivityUnitTest {
+    @Test
+    public void test_userLoginSuccessful() {
+
+    }
+}
