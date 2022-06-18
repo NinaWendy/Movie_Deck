21103dbfffe22e7956bdca40359f2d86f73edb70
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-06-18 18:41:35 +0300

    save items to firebase onclick

--- app/src/main/java/com/moringaschool/movie_deck/ui/activities/HomeActivity.java
+++ app/src/main/java/com/moringaschool/movie_deck/ui/activities/HomeActivity.java
@@ -6,12 +6,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.firebase.database.FirebaseDatabase;
 import com.moringaschool.movie_deck.R;
 import com.moringaschool.movie_deck.models.Movies;
 import com.moringaschool.movie_deck.ui.adapters.RecyclerViewAdapter;
+import com.moringaschool.movie_deck.ui.adapters.RecyclerViewInterface;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements RecyclerViewInterface {
     Movies movies;
     RecyclerView movieRecyclerList;
 
@@ -25,10 +28,17 @@ public class HomeActivity extends AppCompatActivity {
         // Lookup the recyclerview in activity layout
         movieRecyclerList = (RecyclerView) findViewById(R.id.recyclerView);
         //connect recycler view to its adapter
-        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(HomeActivity.this, movies.getResults());
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(this, HomeActivity.this, movies.getResults());
         movieRecyclerList.setAdapter(recyclerViewAdapter);
         //define layout manager
         RecyclerView.LayoutManager layoutManager= new LinearLayoutManager(HomeActivity.this,LinearLayoutManager.HORIZONTAL,false);
         movieRecyclerList.setLayoutManager(layoutManager);
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        String movieName = movies.getResults().get(position).getOriginalTitle();
+        FirebaseDatabase.getInstance().getReference("Favourites").push().child("Movie").setValue(movieName);
+        Toast.makeText(this, "Added to favourites", Toast.LENGTH_SHORT).show();
     }
 }
